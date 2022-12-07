aa3f49be618185381580e581dc4c26b6c487016a
DriSidibe <dsidibe653@gmail.com>
2022-12-07 22:55:27 +0000

    commit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,6 +4,6 @@
 def complex_delete(a_dictionary, value):
     new = a_dictionary.copy()
     for k, v in new.items():
-            if v == value:
-                del a_dictionary[k]
+        if v == value:
+            del a_dictionary[k]
     return (a_dictionary)
