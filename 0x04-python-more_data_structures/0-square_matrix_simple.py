432e568c8bf94799a39d72eba71573579084b5e4
DriSidibe <dsidibe653@gmail.com>
2022-12-07 17:33:08 +0000

    commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            matrix[i][j] = matrix[i][j]**2
-    return matrix
+	nbr_raw = len(matrix)
+	new_matrix = [[x for x in range(len(matrix[0]))] for y in range(nbr_raw)]
+	for i in range(nbr_raw):
+		for j in range(len(matrix[i])):
+			new_matrix[i][j] = matrix[i][j]**2
+	return new_matrix
