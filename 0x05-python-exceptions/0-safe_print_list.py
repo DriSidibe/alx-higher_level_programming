29a4f9a099f143956391695509633d415d7a24ba
DriSidibe <dsidibe653@gmail.com>
2022-12-20 11:53:42 +0000

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-	elm_nbr = 0
-	for i in range(x):
-		try:
-			print(my_list[i], end="")
-			elm_nbr += 1
-		except:
-			print("\n", end="")
-			return elm_nbr
-	print("\n", end="")
-	return elm_nbr
+    elm_nbr = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            elm_nbr += 1
+        except:
+            print("\n", end="")
+            return elm_nbr
+    print("\n", end="")
+    return elm_nbr
