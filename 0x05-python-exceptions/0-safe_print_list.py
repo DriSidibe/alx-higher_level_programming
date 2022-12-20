55b74e0ce29c7365428e39136ee75b7ba16eb4ce
DriSidibe <dsidibe653@gmail.com>
2022-12-20 11:56:04 +0000

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             elm_nbr += 1
-        except:
+        except IndexError:
             print("\n", end="")
             return elm_nbr
     print("\n", end="")
