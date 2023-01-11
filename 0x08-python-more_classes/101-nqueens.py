81997a0696d0154a9423f0750982c5785c1a14ee
DriSidibe <dsidibe653@gmail.com>
2023-01-11 11:40:07 +0000

    commit

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -8,6 +8,9 @@ class Nqueenproblem:
 		self.solutions = []
 		self.is_running = True
 		self.queen_count = 0
+		self.is_found = False
+		self.continue_finding = False
+		self.recursion = False
 		self.i, self.j = 0, 0
 
 	def set_cursor(self):
@@ -54,35 +57,40 @@ class Nqueenproblem:
 			return True
 		return False
 
-	def back(self):
-		if self.cursor[1] == self.N-1:
-			if self.cursor[0] == 0:
-				print("can't solve")
-				self.print_solutions(self.solutions)
-				exit()
-			self.find_new()
-
-	def find_new(self):
-		for i in range(self.N):
-			if self.board[self.cursor[0]-1][i] == 1:
-				if i == self.N-1:
-					self.board[self.cursor[0]-1][i] = 0
-					self.cursor = [self.cursor[0]-1, i]
-					self.find_new()
-				self.board[self.cursor[0]-1][i] = 0
-				self.cursor = [self.cursor[0]-1, i+1]
-				self.solve()
-
 	def solve(self):
+		print("finding...")
 		while self.is_running:
+			self.recursion = False
 			if self.is_attacked():
-				self.back()
-				self.set_cursor()
+				if self.cursor[1] == self.N-1:
+					if self.cursor[0] == 0:
+						print("can't solve")
+						self.print_solutions(self.solutions)
+						exit()
+					self.is_found = False
+					self.continue_finding = False
+					while not self.is_found:
+						for i in range(self.N):
+							if self.board[self.cursor[0]-1][i] == 1:
+								if i == self.N-1:
+									self.board[self.cursor[0]-1][i] = 0
+									self.cursor = [self.cursor[0]-1, i]
+									self.continue_finding = True
+									break
+								self.board[self.cursor[0]-1][i] = 0
+								self.cursor = [self.cursor[0]-1, i+1]
+								self.continue_finding = False
+								self.recursion = True
+								break
+						if not self.continue_finding:
+							self.is_found = True
+				if not self.recursion:
+					self.set_cursor()
 			else:
 				self.board[self.cursor[0]][self.cursor[1]] = 1
 				if self.cursor[0] == self.N-1:
 					self.solutions.append(self.board)
-					break
+					self.is_running = False
 				self.cursor = [self.cursor[0]+1, 0]
 		self.print_solutions(self.solutions)
 
