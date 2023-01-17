97d548f8b9005788f96d3172ca08cbde3c4dba9e
DriSidibe <dsidibe653@gmail.com>
2023-01-17 12:08:16 +0000

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,12 +16,31 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries in [None, []]:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
 
+    @classmethod
     def save_to_file(cls, list_objs):
         with open("Rectangle.json", "w") as file:
-            file.write(to_json_string(map(lambda obj : obj.to_dictionary(), list_objs)))
+            file.write(cls.to_json_string(list(map(lambda obj : obj.to_dictionary(), list_objs))))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string in [None, []]:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        dummy_instance = cls(5, 5)
+        dummy_instance.update(dictionary)
+        return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        pass
