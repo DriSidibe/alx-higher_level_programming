66e47763f6d80045f2f1ada4be9c39dc2c48eee4
DriSidibe <105156435+DriSidibe@users.noreply.github.com>
2023-10-03 09:12:55 +0000

    Delete 0x0D-SQL_introduction directory

--- 0x0D-SQL_introduction/4-first_table.sql
+++
@@ -1,5 +0,0 @@
--- creates a table called firt_table in the current database
--- database will be passed as argument of the mysql command
-CREATE TABLE IF NOT EXISTS first_table (id INT,
-		name VARCHAR(256)
-		);
