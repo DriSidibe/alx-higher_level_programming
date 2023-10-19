6791b167104348896fa5eba4e380b97091662cef
DriSidibe <dsidibe653@gmail.com>
2023-10-19 18:07:04 +0200

    update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- displays all records of the second_table of database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE ALTER TABLE first_table MODIFY name VARCHAR(256) CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
